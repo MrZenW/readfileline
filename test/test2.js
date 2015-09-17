9e57898f36ef447872b46ddd0a72d1247d42133c
zenboss <zenyes@gmail.com>
2015-09-17 20:57:30 +0800

    fixed indent

--- test/test2.js
+++ test/test2.js
@@ -7,14 +7,14 @@ fl(__dirname+'/data.txt',function(lineData,lineNum){
     console.log(lineNum+':'+lineData+'\n');
 },function(err,eventType,totalLineNum){
     if(eventType=='end'){
-    	//if file read all line to success, "close" event still will emit, so you need distinguish between they
-    	console.log('read line is done, total '+totalLineNum+' line(s)');
+        //if file read all line to success, "close" event still will emit, so you need distinguish between they
+        console.log('read line is done, total '+totalLineNum+' line(s)');
     }
-	if(eventType=='close'){
-		console.log('read line is closed, read to '+totalLineNum+' line(s)');
-	}
+    if(eventType=='close'){
+        console.log('read line is closed, read to '+totalLineNum+' line(s)');
+    }
     if(eventType=='error'){
-    	console.log('read file line has an error:'+err.message);
+        console.log('read file line has an error:'+err.message);
     }
 });
 
@@ -22,22 +22,22 @@ fl(__dirname+'/data.txt',function(lineData,lineNum){
 
 var fileContents2=[];
 fl(__dirname+'/data.txt',function(lineData,lineNum){
-	var thisFL = this;
-	if(lineNum>=5){
-		this.close(); //And also has a function is "this.stop()", but it still emit "close" event so you still need listen "close" event, so mustn't listen "stop" event
-		return;
-	}
+    var thisFL = this;
+    if(lineNum>=5){
+        this.close(); //And also has a function is "this.stop()", but it still emit "close" event so you still need listen "close" event, so mustn't listen "stop" event
+        return;
+    }
     fileContents2.push(lineData);
     console.log(lineNum+':'+lineData+'\n');
 },function(err,eventType,totalLineNum){
     if(eventType=='end'){
-    	//if file read all line to success, "close" event still will emit, so you need distinguish between they
-    	console.log('read line is done, total '+totalLineNum+' line(s)');
+        //if file read all line to success, "close" event still will emit, so you need distinguish between they
+        console.log('read line is done, total '+totalLineNum+' line(s)');
     }
-	if(eventType=='close'){
-		console.log('read line is closed, read to '+totalLineNum+' line(s)');
-	}
+    if(eventType=='close'){
+        console.log('read line is closed, read to '+totalLineNum+' line(s)');
+    }
     if(eventType=='error'){
-    	console.log('read file line has an error:'+err.message);
+        console.log('read file line has an error:'+err.message);
     }
 });
